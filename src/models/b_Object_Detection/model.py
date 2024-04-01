af7458e54701cfef78031142449f48be57444a04
unknown <yasamin.nourijelyani@mail.utoronto.ca>
2024-03-31 21:36:37 -0400

    fixed error

--- src/models/b_Object_Detection/model.py
+++ src/models/b_Object_Detection/model.py
@@ -6,7 +6,7 @@ class EncoderCNN(nn.Module):
     def __init__(self, embed_size, train_CNN=False):
         super(EncoderCNN, self).__init__()
         self.train_CNN = train_CNN
-        self.inception = models.inception_v3(pretrained=True, aux_logits=False)
+        self.inception = models.inception_v3(pretrained=True, aux_logits=True)
         self.inception.fc = nn.Linear(self.inception.fc.in_features, embed_size)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.5)
