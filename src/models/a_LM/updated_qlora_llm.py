6e2b24934471a91e3bf8268e1513d4fdb602489f
unknown <yasamin.nourijelyani@mail.utoronto.ca>
2024-04-02 16:27:52 -0400

    fixed bug

--- src/models/a_LM/updated_qlora_llm.py
+++ src/models/a_LM/updated_qlora_llm.py
@@ -66,7 +66,7 @@ def fine_tune(output_dir, model, lr=2e-4, batch_size=64, num_epochs=10):
     # data_train = load_dataset("json", data_files=data_path_train)
     # # tokenize training and validation datasets
     tokenized_data = data.map(tokenize_function, batched=True)
-    print(type(tokenized_data))
+    print(tokenized_data["test"])
   
 
 
