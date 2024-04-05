b478f6976baf9e72fa3b7a234078eb8becd8bbee
unknown <yasamin.nourijelyani@mail.utoronto.ca>
2024-04-05 12:40:24 -0400

    changed output loc

--- src/models/b_Object_Detection/train.py
+++ src/models/b_Object_Detection/train.py
@@ -20,8 +20,8 @@ def train():
     )
 
     train_loader, _, train_dataset, _ = get_data_loaders(
-        train_annotations_file="../../../data/train_test_data/train_imgloc_caption.jsonl",
-        test_annotations_file="../../../data/train_test_data/test_imgloc_caption.jsonl",
+        train_annotations_file="../../../data/train_imgloc_caption.jsonl",
+        test_annotations_file="../../../data/test_imgloc_caption.jsonl",
         root_folder="../../../data",
         transform=transform
     )
