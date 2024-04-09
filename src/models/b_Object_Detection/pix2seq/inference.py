2791ba05ebd9188a760c1c59dbf7f4df43b1a17a
unknown <yasamin.nourijelyani@mail.utoronto.ca>
2024-04-09 16:34:02 -0400

    fixed inference

--- src/models/b_Object_Detection/pix2seq/inference.py
+++ src/models/b_Object_Detection/pix2seq/inference.py
@@ -1,34 +1,7 @@
-
-import gc
-import os
-import cv2
-import math
-import random
-from glob import glob
-import numpy as np
-import pandas as pd
-from functools import partial
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-import albumentations as A
-import xml.etree.ElementTree as ET
-from sklearn.model_selection import StratifiedGroupKFold
 import torch
-from torch import nn
-import torch.nn.functional as F
-from torch.nn.utils.rnn import pad_sequence
-import cv2
-from torch.utils.data import Dataset
-import timm
-from timm.models.layers import trunc_normal_
-import transformers
-from transformers import top_k_top_p_filtering
-from transformers import get_linear_schedule_with_warmup
-import albumentations as A
+from transformers.generation_utils import top_k_top_p_filtering
 from config import CFG
-import utils
-from tokenizer import KeypointTokenizer, get_loaders
-from model import Encoder, Decoder, EncoderDecoder
+
 
 
 def generate(model, x, tokenizer, max_len=50, top_k=0, top_p=1):
@@ -44,7 +17,7 @@ def generate(model, x, tokenizer, max_len=50, top_k=0, top_p=1):
     with torch.no_grad():
         for i in range(max_len):
             preds = model.predict(x, batch_preds)
-            ## If top_k and top_p are set to default, the following line does nothing!
+            ## If top_k and top_p are set to default, the following line does nothing
             preds = top_k_top_p_filtering(preds, top_k=top_k, top_p=top_p)
             if i % 4 == 0:
                 confs_ = torch.softmax(preds, dim=-1).sort(axis=-1, descending=True)[0][:, 0].cpu()
@@ -61,22 +34,22 @@ def postprocess(batch_preds, batch_confs, tokenizer):
     invalid_idxs = ((EOS_idxs - 1) % 5 != 0).nonzero().view(-1)
     EOS_idxs[invalid_idxs] = 0
     
-    all_bboxes = []
+    all_keypoints = []
     all_labels = []
     all_confs = []
     for i, EOS_idx in enumerate(EOS_idxs.tolist()):
         if EOS_idx == 0:
-            all_bboxes.append(None)
+            all_keypoints.append(None)
             all_labels.append(None)
             all_confs.append(None)
             continue
-        labels, bboxes = tokenizer.decode(batch_preds[i, :EOS_idx+1])
-        confs = [round(batch_confs[j][i].item(), 3) for j in range(len(bboxes))]
+        labels, keypoints = tokenizer.decode(batch_preds[i, :EOS_idx+1])
+        confs = [round(batch_confs[j][i].item(), 3) for j in range(len(keypoints))]
         
-        all_bboxes.append(bboxes)
+        all_keypoints.append(keypoints)
         all_labels.append(labels)
         all_confs.append(confs)
         
-    return all_bboxes, all_labels, all_confs
+    return all_keypoints, all_labels, all_confs
 
 
