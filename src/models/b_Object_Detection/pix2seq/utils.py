964c7fcd0185d3077aaf47806c016849f672060d
unknown <yasamin.nourijelyani@mail.utoronto.ca>
2024-04-09 15:36:05 -0400

    updated imports

--- src/models/b_Object_Detection/pix2seq/utils.py
+++ src/models/b_Object_Detection/pix2seq/utils.py
@@ -1,23 +1,4 @@
-import cv2
-import numpy as np
-import pandas as pd
-from functools import partial
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-import albumentations as A
-import xml.etree.ElementTree as ET
-from sklearn.model_selection import StratifiedGroupKFold
 import torch
-from torch import nn
-import torch.nn.functional as F
-from torch.nn.utils.rnn import pad_sequence
-import cv2
-from torch.utils.data import Dataset
-from timm.models.layers import trunc_normal_
-import transformers
-from transformers import top_k_top_p_filtering
-from transformers import get_linear_schedule_with_warmup
-import albumentations as A
 from config import CFG
 
 def generate_square_subsequent_mask(sz):
